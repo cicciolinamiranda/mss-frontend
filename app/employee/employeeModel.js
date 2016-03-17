481d97e1f5367a15f4dabd5d5b7dc018c13fca7b
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2016-03-17 21:34:32 +0800

    initial model code

--- app/employee/employeeModel.js
+++ app/employee/employeeModel.js
@@ -1,7 +1,12 @@
 module.exports = function(ngModule) {
-  ngModule.service('Employee', employeeModel);
+  ngModule.factory('Employee', employeeModel);
 };
 
 function employeeModel() {
   "ngInject";
+
+  function Employee() {
+  }
+
+  return Employee;
 }
