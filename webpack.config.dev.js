eb268d1be5079bdc722de398cdf5892f56800a13
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2016-03-17 09:22:07 +0800

    configure webpack for loading css

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -10,7 +10,8 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.js$/, loaders: 'ng-annotate', exclude: /node_modules/},
-      {test: /\.html$/, loaders: 'raw', exclude: /node_modules/}
+      {test: /\.html$/, loaders: 'raw', exclude: /node_modules/},
+      {test: /\.css$/, loaders: 'style!css', exclude: /node_modules/}
     ]
   }
 };
