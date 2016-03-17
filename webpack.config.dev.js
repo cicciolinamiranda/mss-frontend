fa1d22deca1a84b91fb8376ac26582af61c093a2
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2016-03-17 17:59:29 +0800

    resolve bower components from webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -13,5 +13,13 @@ module.exports = {
       {test: /\.html$/, loader: 'raw', exclude: /node_modules/},
       {test: /\.css$/, loader: 'style!css', exclude: /node_modules/}
     ]
-  }
+  },
+  resolve: {
+    root: [path.join(__dirname, 'bower_components')]
+  },
+  plugins: [
+    new webpack.ResolverPlugin(
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('.bower.json', ['main'])
+    )
+  ]
 };
