daa71bb99361152d7cf39dd3b11a6018cc46371f
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2016-03-20 11:59:08 +0800

    webpack file loader for static assets

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -10,8 +10,9 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.js$/, loader: 'ng-annotate', exclude: /node_modules/},
+      {test: /\.css$/, loader: 'style!css', exclude: /node_modules/},
       {test: /\.html$/, loader: 'raw', exclude: /node_modules/},
-      {test: /\.css$/, loader: 'style!css', exclude: /node_modules/}
+      {test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/, loader: 'file', exclude: /node_modules/}
     ]
   }
 };
