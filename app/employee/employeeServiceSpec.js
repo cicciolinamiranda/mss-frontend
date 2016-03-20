6b359027ed034fd8c57f32569eed4cea12425a9b
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2016-03-20 12:24:59 +0800

    prelim testing setup

---
+++ app/employee/employeeServiceSpec.js
@@ -0,0 +1,19 @@
+describe("Employee Service", function() {
+  var EmployeeSvc;
+  beforeEach(module('app'));
+  beforeEach(inject(function(_EmployeeSvc_){
+    EmployeeSvc = _EmployeeSvc_;
+  }));
+
+  describe("List employees", function() {
+    it("returns a promise that resolves with an array of employees", function() {
+      //
+    });
+  });
+
+  describe("Get employee", function() {
+    it("returns a promise that resolves with an employee object", function() {
+      //
+    });
+  });
+});
