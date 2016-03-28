e2d2927a587094ed815be19bf3f304a0fcd5cc4b
darryl.ong <darryl.ong@cloudsherpas.com>
2016-03-28 21:49:44 +0800

    Added datepicker and logic for start and end dates

--- app/work-order/location/create/createController.js
+++ app/work-order/location/create/createController.js
@@ -1,12 +1,15 @@
 module.exports = viewCtrl;
 
+var moment = require('moment');
+
 function viewCtrl() {
   "ngInject";
   var _this = this;
   _this.location = {};
 
   function init() {
-  	
+    console.log("DAWI!");
+  	_this.location.startDate = moment().toDate();
   }
 
   init();
