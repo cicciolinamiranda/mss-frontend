188bba2c9283780f4326d1d5eb0e29032aff5907
Jordan Duabe <jordan.duabe@cloudsherpas.com>
2016-03-31 16:15:17 +0800

    Create saveCustomerLocation() service method
    Implemented the saving of customer location through the customer
    location backend api.
    
    JP-4410

--- app/work-order/location/create/createService.js
+++ app/work-order/location/create/createService.js
@@ -1,8 +1,8 @@
-module.exports = function(ngModule) {
+module.exports = function (ngModule) {
   ngModule.service('CreateLocationSvc', createLocationService);
 };
 
-function createLocationService($http, $q) {
+function createLocationService($http, $q, $gapi) {
 
   var _this = this;
   _this.modeOfTransportMock;
@@ -14,61 +14,85 @@ function createLocationService($http, $q) {
   _this.getBilledCostTypeValues = getBilledCostTypeValues;
   _this.searchSiteSkills = searchSiteSkills;
   _this.searchProtectiveEquipment = searchProtectiveEquipment;
+  _this.saveCustomerLocation = saveCustomerLocation;
 
-  function searchMockModeOfTransport(keyword){
+  function searchMockModeOfTransport(keyword) {
     var def = $q.defer();
 
-    $http.get("http://localhost:3000/modeOfTransport", {params:{"q": keyword}})
-         .success(function(response) {
-              _this.modeOfTransportMock = response;
-              def.resolve(response);
-            })
-          .error(function() {
-              def.reject("Server is down.");
-          });
+    $http.get("http://localhost:3000/modeOfTransport", {params: {"q": keyword}})
+        .success(function (response) {
+          _this.modeOfTransportMock = response;
+          def.resolve(response);
+        })
+        .error(function () {
+          def.reject("Server is down.");
+        });
     return def.promise;
   }
 
-  function getBilledCostTypeValues(){
+  function getBilledCostTypeValues() {
     var def = $q.defer();
 
     $http.get("http://localhost:3000/billedCostType")
-         .success(function(response) {
-              _this.billedCostType = response;
-              def.resolve(response);
-            })
-          .error(function() {
-              def.reject("Server is down.");
-          });
+        .success(function (response) {
+          _this.billedCostType = response;
+          def.resolve(response);
+        })
+        .error(function () {
+          def.reject("Server is down.");
+        });
     return def.promise;
   }
 
-  function searchSiteSkills(keyword){
+  function searchSiteSkills(keyword) {
     var def = $q.defer();
 
-    $http.get("http://localhost:3000/skills", {params:{"q": keyword}})
-         .success(function(response) {
-              _this.siteSkills = response;
-              def.resolve(response);
-            })
-          .error(function() {
-              def.reject("Server is down.");
-          });
+    $http.get("http://localhost:3000/skills", {params: {"q": keyword}})
+        .success(function (response) {
+          _this.siteSkills = response;
+          def.resolve(response);
+        })
+        .error(function () {
+          def.reject("Server is down.");
+        });
     return def.promise;
   }
 
 
-  function searchProtectiveEquipment(keyword){
+  function searchProtectiveEquipment(keyword) {
     var def = $q.defer();
 
-    $http.get("http://localhost:3000/equipments", {params:{"q": keyword}})
-         .success(function(response) {
-              _this.siteSkills = response;
-              def.resolve(response);
-            })
-          .error(function() {
-              def.reject("Server is down.");
-          });
+    $http.get("http://localhost:3000/equipments", {params: {"q": keyword}})
+        .success(function (response) {
+          _this.siteSkills = response;
+          def.resolve(response);
+        })
+        .error(function () {
+          def.reject("Server is down.");
+        });
     return def.promise;
+  }
+
+  function saveCustomerLocation(customerLocationDetails) {
+    var cache = {};
+    var deferred = $q.defer();
+    var loadApi = deferred.promise;
+
+    $gapi.loaded.then(function () {
+      return $gapi.load('workorder', 'v1', true);
+    }).then(function () {
+      return deferred.resolve();
+    });
+
+    this.get = function (id) {
+      var deferred2 = $q.defer();
+      loadApi.then(function () {
+        return $gapi.client.workorder.customer.location.add({customerLocationDTO: customerLocationDetails});
+      }).then(function (data) {
+        angular.extend(cache[id], data);
+        deferred2.resolve(cache[id]);
+      });
+      return deferred2.promise;
+    };
   }
 }
