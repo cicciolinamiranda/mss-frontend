144012a58ca26b9321661f5028cd47e4b415878a
Cicciolina Miranda <cicciolina.miranda@cloudsherpas>
2016-04-02 03:57:58 +0800

    JP-4489- integrate view custloc to the backendpoint

--- app/work-order/location/view/viewLocationService.js
+++ app/work-order/location/view/viewLocationService.js
@@ -2,7 +2,7 @@ module.exports = function(ngModule) {
   ngModule.service('ViewLocationSvc', viewLocationService);
 };
 
-function viewLocationService($http, $q) {
+function viewLocationService($http, $q, $gapi) {
 
   var _this = this;
 
@@ -10,17 +10,34 @@ function viewLocationService($http, $q) {
   _this.getBilledCostType = getBilledCostType;
   _this.locDetails;
 
+  var deferred = $q.defer();
+  var loadApi = deferred.promise;
+
+  $gapi.loaded.then(function () {
+    return $gapi.load('workorder', 'v1', true);
+  }).then(function () {
+    return deferred.resolve();
+  });
+
+
   function getLocationDetails(id){
     var def = $q.defer();
 
-    $http.get("http://localhost:3000/customerLocation", {params:{"id": id}})
-         .success(function(response) {
-              _this.locDetails = response;
-              def.resolve(response);
-            })
-          .error(function() {
-              def.reject("Server is down.");
-          });
+    // $http.get("http://localhost:3000/customerLocation", {params:{"id": id}})
+    //      .success(function(response) {
+    //           _this.locDetails = response;
+    //           def.resolve(response);
+    //         })
+    //       .error(function() {
+    //           def.reject("Server is down.");
+    //       });
+
+    loadApi.then(function () {
+      return $gapi.client.workorder.customer.location.get({'id' : id});
+      def.resolve(response);
+    }).then(function (data) {
+      def.resolve(data);
+    });
     return def.promise;
   }
 
