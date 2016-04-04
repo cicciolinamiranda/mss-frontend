e44588bd1a9e758309d053e98525308805b756b9
darryl.ong <darryl.ong@cloudsherpas.com>
2016-04-04 17:44:21 +0800

    Design Overhaul based on UI Team Design CSS and fonts

--- app/routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(ngModule) {
-  ngModule.config(routes);
-};
-
-function routes($urlRouterProvider) {
-  // the default state referencing a module
-  $urlRouterProvider.otherwise('/employees');
-}
