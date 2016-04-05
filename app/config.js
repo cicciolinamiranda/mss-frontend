e7a2898658882607de2a31b0e37365c0a928fddf
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2016-04-05 15:03:51 +0800

    point to new JavelinQA backend

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@ module.exports = function(ngModule) {
 
 function config($httpProvider, GAuthProvider) {
   $httpProvider.defaults.withCredentials = true;
-  GAuthProvider.authUrl = 'https://employee-backend-dot-cs-javelin-mss-team-2.appspot.com/login/google';
+  GAuthProvider.authUrl = 'https://javelin-qa.appspot.com/login/google';
   GAuthProvider.destinationStateOnSuccess = 'employee.list';
   GAuthProvider.destinationStateOnError = 'auth.error';
 }
--- app/employee/config.js
+++ app/employee/config.js
@@ -1,3 +1,3 @@
 module.exports = function(ngModule) {
-  ngModule.constant('EMPLOYEE_GAPI_BASE','https://employee-backend-dot-cs-javelin-mss-team-2.appspot.com/_ah/api');
+  ngModule.constant('EMPLOYEE_GAPI_BASE','https://javelin-qa.appspot.com/_ah/api');
 };
