c750faed444539ca9d3111a40f5a9b6d04e2e453
Cicciolina Miranda <cicciolina.miranda@cloudsherpas>
2016-04-07 19:24:48 +0800

    change to GAPI base to version 5

--- app/search/config.js
+++ app/search/config.js
@@ -1,3 +1,3 @@
 module.exports = function(ngModule) {
-  ngModule.constant('WORKORDER_GAPI_BASE','https://3-dot-cs-development-playground.appspot.com/_ah/api');
+  ngModule.constant('WORKORDER_GAPI_BASE','https://5-dot-cs-development-playground.appspot.com/_ah/api');
 };
