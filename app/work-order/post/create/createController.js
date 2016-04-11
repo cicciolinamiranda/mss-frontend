d56b4759d5593a316795218f24f0c46e8eb7571c
Sharlyn Mae Pandes <sharlyn.pandes@cloudsherpas.com>
2016-04-11 19:28:33 +0800

    Refactor of PostModel.js and application in create post page

--- app/work-order/post/create/createController.js
+++ app/work-order/post/create/createController.js
@@ -5,17 +5,31 @@ var moment = require('moment');
 /*@ngInject*/
 function createCtrl($state, $stateParams, PostModel, CreatePostSvc) {
   var _this = this;
-  _this.model = PostModel;
+  var locationId = $stateParams.locationId;
+  _this.model = new PostModel();
 
   _this.save = save;
-
+  _this.genderChoices = [];
   _this.selectedTraining;
+  // _this.model.post.customerLocationId = $stateParams.locationId;
 
-  console.log('LocationId',$stateParams.locationId);
-  _this.model.post.customerLocationId = $stateParams.locationId;
+  function init() {
+    _this.post = _this.model.post;
+    _this.post.customerLocationId = locationId;
+    _this.callInFrequencyChoices = _this.model.callInFrequencyChoices;
+
+    _this.model.getGenderChoices().then(function (response) {
+      _this.genderChoices = response;
+      if(_this.genderChoices && _this.genderChoices){
+        _this.post.preferences.gender = _this.genderChoices[0];
+      }
+    });
+  }
+
+  init();
 
   function save(){
-    CreatePostSvc.save(PostModel.getPostInDtoFormat()).then(function(response){
+    CreatePostSvc.save(PostModel.getPostInDtoFormat(_this.post)).then(function(response){
       var postId = response.id;
       console.log('Post ID',postId);
       $state.go('post.view', {id: postId});
