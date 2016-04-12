c750db180f07a37e1722484c564b5a325322b57a
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2016-04-12 21:20:02 +0800

    create gapi request decorator for protecting access to pages

--- app/auth/config.js
+++ app/auth/config.js
@@ -4,5 +4,5 @@ module.exports = function(ngModule) {
 
 function config($gapiProvider) {
   $gapiProvider.client_id = '346709668681-td68i35jnl7f40kdlmdgnj0gr2hcg2ua.apps.googleusercontent.com';
-  $gapiProvider.scopes = ["email", "profile"];
+  $gapiProvider.scopes = ['email', 'profile'];
 }
