2ed04369d02d150602eae0ca874886d9ae806091
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2016-04-13 19:15:51 +0800

    differentiate ui when searching

--- app/employee/list/listController.js
+++ app/employee/list/listController.js
@@ -3,6 +3,9 @@ module.exports = listCtrl;
 /*@ngInject*/ function listCtrl(EmployeeListSvc, $stateParams) {
   var _this = this;
 
+  _this.isSearch = !!$stateParams.searchTerm;
+  _this.searchTerm = $stateParams.searchTerm;
+
   function init() {
     EmployeeListSvc.list({q: $stateParams.searchTerm}).then(function(employees){
       _this.employees = employees;
