19066df7eae4e572bfd804a01a8a32e72dbc3885
sulromares <sulpicio.romares@cloudsherpas.com>
2016-04-15 01:11:40 +0800

    add unit test on listComponentSpec

---
+++ app/employee/list/listComponentSpec.js
@@ -0,0 +1,64 @@
+var component = require('./index');
+var GAuth = require("../../auth/authProvider.js");
+
+describe("Employee List Component", function() {
+  var $compile, $rootScope;  
+  var sample_employees = [
+    {
+      id: '123',
+      firstname: 'Auntie',
+      surname: 'Anne'
+    },
+    {
+      id: '124',
+      firstname: 'John',
+      surname: 'Walkman'
+    }
+  ];
+  
+  beforeEach(angular.mock.module(GAuth.name));
+  beforeEach(angular.mock.module(component.name));  
+  beforeEach(function() {
+    angular.mock.module(function($provide) {
+      $provide.service('GAuth', function () {
+        return {
+          protect: function(f) {
+            return f;
+          }
+        };        
+      });
+      $provide.constant('EMPLOYEE_GAPI_BASE', '');
+      // mock $gapi to inject to EmployeeViewSvc
+      $provide.service('$gapi', function($q) {
+        var gapi = {
+          loaded: $q.resolve(),
+          load: function() {
+            return $q.resolve();
+          },
+          client: {
+            employee: {
+              employees: {
+                list: function() {
+                  return $q.resolve({employees: sample_employees});
+                }
+              }
+            }
+          }
+        };
+        return gapi;
+      });      
+    });
+  });
+  beforeEach(angular.mock.inject(function(_$compile_, _$rootScope_){
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
+
+  it("contains a list of employees resolved by the service", function() {
+    var element = $compile("<employee-list></employee-list>")($rootScope);
+    $rootScope.$digest();
+    expect(element.html()).toContain("Auntie");
+    expect(element.html()).toContain("Walkman");
+    expect(element.html()).not.toContain("Mark");
+  });
+});
