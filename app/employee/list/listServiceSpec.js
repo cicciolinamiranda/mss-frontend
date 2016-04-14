223f880d39638ee421cce0d3c6230759d02accc7
sulromares <sulpicio.romares@cloudsherpas.com>
2016-04-15 00:49:15 +0800

    Fix failing unit test on Employee listServiceSpec
    Add mocking of GAuth

--- app/employee/list/listServiceSpec.js
+++ app/employee/list/listServiceSpec.js
@@ -1,18 +1,32 @@
 var component = require('./index');
 
+var GAuth = require("../../auth/authProvider.js");
+
 describe("Employee List Service", function() {
   var EmployeeListSvc, $scope;
   // since $gapi already injects $q,
   // it will be implicitly injected to the service
+  var FILTER_ALL = { q:"" };
   var sample_employee = {
     id: '123',
     firstname: 'Auntie',
     surname: 'Anne'
-  };
+  };  
+
+  beforeEach(angular.mock.module(GAuth.name));
   beforeEach(angular.mock.module(component.name));
   // mock $gapi to inject to EmployeeListSvc
   beforeEach(function() {
     angular.mock.module(function($provide) {
+      
+      $provide.service('GAuth', function () {
+        return {
+          protect: function(f) {
+            return f;
+          }
+        };        
+      });
+
       $provide.constant('EMPLOYEE_GAPI_BASE', '');
       $provide.service('$gapi', function($q) {
         var gapi = {
@@ -23,8 +37,8 @@ describe("Employee List Service", function() {
           client: {
             employee: {
               employees: {
-                list: function() {
-                  return $q.resolve({employees: [sample_employee]});
+                list: function() {                 
+                  return $q.resolve({employees: [sample_employee]});  
                 }
               }
             }
@@ -34,23 +48,26 @@ describe("Employee List Service", function() {
       });
     });
   });
+
   beforeEach(angular.mock.inject(function(_EmployeeListSvc_, $injector){
     EmployeeListSvc = _EmployeeListSvc_;
     $scope = $injector.get('$rootScope').$new();
   }));
+
+
   beforeEach(function() {
     spyOn(EmployeeListSvc, 'list').and.callThrough();
   });
 
-  // describe("List employees", function() {
-  //   it("returns a promise that resolves with list of employees", function(done) {
-  //     EmployeeListSvc.list().then(function(employees) {
-  //       expect(employees[0].firstname)
-  //         .toBe(sample_employee.firstname);
-  //       done();
-  //     });
-  //     expect(EmployeeListSvc.list).toHaveBeenCalled();
-  //     $scope.$digest();
-  //   });
-  // });
+  describe("List employees", function() {
+    it("returns a promise that resolves with list of employees", function(done) {
+        EmployeeListSvc.list(FILTER_ALL).then(function(employees) {
+        expect(employees[0].firstname)
+          .toBe(sample_employee.firstname);
+        done();
+      });
+      expect(EmployeeListSvc.list).toHaveBeenCalled();
+      $scope.$digest();
+    });
+  });
 });
