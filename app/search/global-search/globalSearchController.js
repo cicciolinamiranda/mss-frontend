7da8529961bcdd7b5e22a07d31e73e5835936dc1
Roanee Bryle Joseph <roaneebryle.joseph@cloudsherpas.com>
2016-04-16 00:58:34 +0800

    removed console logs

--- app/search/global-search/globalSearchController.js
+++ app/search/global-search/globalSearchController.js
@@ -9,10 +9,8 @@ function globalSearchCtrl(SearchService) {
   _this.selectFilter = SearchService.selectFilter;
 
   _this.search = function() {
-    console.log(_this.searchTerm);
     if(_this.searchTerm == "") {
       _this.emptyError = true;
-      console.log("Error True");
     }
     else _this.emptyError = false;
     SearchService.search(_this.searchTerm)
