8c27e6d2a320129ff89f075f7dcefd76b43235dd
Roanee Bryle Joseph <roaneebryle.joseph@cloudsherpas.com>
2016-04-16 00:54:18 +0800

    JP-4874: Changed search field message according to QA expected result.

--- app/search/global-search/globalSearchController.js
+++ app/search/global-search/globalSearchController.js
@@ -8,6 +8,14 @@ function globalSearchCtrl(SearchService) {
   _this.selectedFilter = SearchService.selectedFilter;
   _this.selectFilter = SearchService.selectFilter;
 
-  _this.search = function() { SearchService.search(_this.searchTerm) };
+  _this.search = function() {
+    console.log(_this.searchTerm);
+    if(_this.searchTerm == "") {
+      _this.emptyError = true;
+      console.log("Error True");
+    }
+    else _this.emptyError = false;
+    SearchService.search(_this.searchTerm)
+  };
   _this.searchTerm = '';
 }
